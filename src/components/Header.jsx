b76c3abbd5e2260005a06c79ba7911c578927ce3
Gustavo  Guimarães <gustavoguimaraes@Mac-mini-de-Innovalab-7.local>
2026-08-18 21:34:56 -0300

    Salvamento2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-// import NavBar from "./Navbar"
-// import Sidebar from "./Sidebar"
+import NavBar from "./Navbar"
+import Sidebar from "./Sidebar"
 
-// export default function Header() {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
+export default function Header() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-//   return (
-//     <header>
-//       <NavBar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+  return (
+    <header>
+      <NavBar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
       
-//       <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)}/>
-//     </header>
-//   )
-// }
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)}/>
+    </header>
+  )
+}
